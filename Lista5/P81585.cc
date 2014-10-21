93fce9f1eadbb7cf47111593bf4188e991b4e341
Awais Iqbal <awais.iqbal2@gmail.com>
2014-10-21 15:46:27 +0200

    added exercici List5

--- Lista5/P81585.cc
+++ Lista5/P81585.cc
@@ -4,19 +4,22 @@ using namespace std;
 int main(){
 	int num = 0;
 	while(cin >> num){
-		int nTotal = 0;
-		for(int i=0;i < num ; ++i){
-			nTotal = nTotal*10 + num;
+		
+		int sum = 0; //sum of all the numers introduced
+		int max = 0; //the max number in introduced
+		for(int i = 0;i < num ; ++i){ 
+			int n = 0;
+			cin >> n;
+			sum+=n;
+			if(n > max)
+				max=n;
 		}
-		bool found = false;
-		int sumPostN = 0;
-		for(int i = num; not found and i > 0 ; ++i){
-			int sumPreN = 0
-			int nPre = nTotal
-			for(int j = 0 ; j < i ; ++j){
-				
-			}
-			
+		//chekc if the max is equal to the sum of the rest.
+		bool found = (sum  - max) == max;
+		if(found){
+			cout << "YES" << endl;
+		}else{
+			cout << "NO" << endl;
 		}
 		
 		
