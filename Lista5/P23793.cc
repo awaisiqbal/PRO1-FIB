a8e504dc6d765065d618a7516aa1691ebf82cf36
Awais Iqbal <awais.iqbal2@gmail.com>
2014-10-23 08:25:23 +0200

    added the date file

--- Lista5/P23793.cc
+++ Lista5/P23793.cc
@@ -1,15 +1,39 @@
 #include <iostream>
 using namespace std;
 
+int compara(int d1, int m1, int a1, int d2, int m2, int a2){
+	int ret = -1;
+	if(a1 > a2){
+		ret = 1;
+	} else if (a1 == a2){
+		if(m1 > m2){
+			ret = 1;
+		} else if( m1 == m2){
+			if(d1 > d2){
+				ret = 1;
+			} else if( d1 == d2){
+				ret = 0;
+			}
+		}
+	}
+	return ret;
+}
 
 int main(){
 	int d1,m1,a1,d2,m2,a2;
 	char c;
-	
+	while(cin >> d1 >> c >> m1 >> c >> a1 >> c >> d2 >> c >> m2 >> c >> a2 ){
+		int comp = compara( d1,  m1,  a1,  d2,  m2,  a2);
+		if(comp < 0){
+			cout << "anterior" << endl;
+		}else if (comp == 0){
+			cout << "iguals" << endl;
+		} else {
+			cout << "posterior" << endl;
+		}
+	}
 	cin >> d1 >> c >> m1 >> c >> a1;
 	cin >> d2 >> c >> m2 >> c >> a2;
 	
 	
-	cout << d1 << "/" << m1 << "/" << a1 << endl;
-	cout << d2 << "/" << m2 << "/" << a2 << endl;
 }
