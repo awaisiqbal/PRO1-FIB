4fe3eccc914264d967837a82332db1256e2f3640
Awais Iqbal <awais.iqbal2@gmail.com>
2014-10-24 22:27:03 +0200

    List 5 finished

--- Lista5/P23793.cc
+++ Lista5/P23793.cc
@@ -2,6 +2,7 @@
 using namespace std;
 
 int compara(int d1, int m1, int a1, int d2, int m2, int a2){
+	//cout << d1 << "/" << m2 << "/" << a1 << "		" << d2 << "/" << m2 << "/" << a2 << endl; 
 	int ret = -1;
 	if(a1 > a2){
 		ret = 1;
@@ -22,7 +23,7 @@ int compara(int d1, int m1, int a1, int d2, int m2, int a2){
 int main(){
 	int d1,m1,a1,d2,m2,a2;
 	char c;
-	while(cin >> d1 >> c >> m1 >> c >> a1 >> c >> d2 >> c >> m2 >> c >> a2 ){
+	while(cin >> d1 >> c >> m1 >> c >> a1 >> d2 >> c >> m2 >> c >> a2 ){
 		int comp = compara( d1,  m1,  a1,  d2,  m2,  a2);
 		if(comp < 0){
 			cout << "anterior" << endl;
