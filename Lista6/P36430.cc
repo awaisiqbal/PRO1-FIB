dd9cd066b5f9ec939ca69d90f3707c6a58850176
Awais Iqbal <awais.iqbal2@gmail.com>
2014-10-29 21:29:38 +0100

    list 6 complt

--- Lista6/P36430.cc
+++ Lista6/P36430.cc
@@ -2,17 +2,30 @@
 #include <math.h>
 using namespace std;
 
-
-int main(){
-	int a,b,c,d;
-	cin >> a >> b >> c >> d;
+bool calculateFermat(int a, int b, int c, int d, int &x, int &y, int &z){
 	
 	for(int i = a;i <= b;++i){
-		for(int j = c; c <= d;++j){
+		for(int j = c; j <= d;++j){	
 			int calc = sqrt(i*i+j*j);
-			//if(calc == ceil(calc))
-			cout << calc << endl;
+			if(calc*calc == i*i + j*j){
+				x = i;
+				y = j;
+				z = calc;
+				return true;
+			}
 			
 		}
+	}
+	return false;
+}
+
+int main(){
+	int a,b,c,d,x,y,z;
+	cin >> a >> b >> c >> d;
+	bool founded = calculateFermat(a,b,c,d,x,y,z);
+	if(founded){
+		cout << x << "^2 + " << y << "^2 = " << z << "^2" << endl;
+	} else {
+		cout << "No solution!" << endl;;
 	}
 }
